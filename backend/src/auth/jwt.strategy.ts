9edff8e0e2d74e5d8c1d9c28c8b76e7a3bbc14b4
sanaullahbugti <sanaullahbugti821@gmail.com>
2024-05-06 05:42:43 +0500

    easygenerator test task completed

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -12,7 +12,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {  // 'jwt' i
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: "abc778888"
+      secretOrKey: "abc778888" // This should be in env file which will be conmmented in git or ssm params in aws or env variables in heruku
     });
   }
 
